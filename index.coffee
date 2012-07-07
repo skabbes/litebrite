e86ebc7052ea014c52b54e5cbced0b06ef0e100a
Steven Kabbes <stevenkabbes@gmail.com>
2012-07-06 18:33:30 -0700

    Helper visualization for getting around brightness glitch

--- index.coffee
+++ index.coffee
@@ -3,9 +3,11 @@ ctx = null
 radius = 500
 
 rgbToHsv = (r, g, b) ->
+  ###
   r = r / 0xF
   g = g / 0xF
   b = b / 0xF
+  ###
 
   min = Math.min(r, g, b)
   max = Math.max(r, g, b)
@@ -39,9 +41,128 @@ rgbToHsv = (r, g, b) ->
 ready = () ->
   canvas = document.getElementById('wheel')
   ctx = canvas.getContext('2d')
+  # window.addEventListener("resize", draw, false)
+
+  value = 0
+  setInterval () ->
+    draw value
+    value++
+    if value > 15
+      value = 0
+  , 500
+  return
+
+  #
+  hues = []
+  for red in [0..0xF]
+    for green in [0..0xF]
+      for blue in [0..0xF]
+        hues.push rgbToHsv(red, green, blue)[0]
+
+  console.log _.uniq(hues)
+
+  i = 0
+  setInterval () ->
+    i = drawPaths(i)
+  , 100
+
+drawPaths = (start) ->
+  canvas.width = canvas.width
+  allPoints = []
+  outerPoints = []
+
+  drawSomething (r, g, b) ->
+    [h, s, v] = rgbToHsv(r, g, b)
+
+    x = radius + (radius * s / 0xF) * Math.cos(h)
+    y = radius + (radius * s / 0xF) * Math.sin(h)
+    obj =
+      hsv : [h, s, v]
+      rgb : [r, g, b]
+      xy  : [x, y]
+    allPoints.push(obj)
+
+  vObj = {}
+  allPoints.forEach (point) ->
+    [h, s, v] = point.hsv
+    vObj[v] = true
+  vs = Object.keys(vObj)
+
+
+
+  vs.sort()
+  vs = vs.reverse()
+
+  allPoints.forEach (point) ->
+    [h, s, v] = point.hsv
+    if v == 15
+      outerPoints.push(point)
+
+  outerPoints = _.sortBy outerPoints, (p) ->
+    return p.hsv[0]
+  console.log outerPoints.length
+
+  if start > outerPoints.length - 2
+    start = 0
+
+  outerPoints.slice(start, start+1).forEach (p) ->
+    prevClosest = closestPointWithValueAndHue(allPoints, p.hsv[2] - 1, p.hsv[0])
+    for i in [2..15]
+      closest = closestPointWithValueAndHue(allPoints, p.hsv[2] - i, p.hsv[0])
+      if !closest?
+        return
+
+      ctx.beginPath()
+      [x, y] = prevClosest.xy
+      ctx.moveTo(x,y)
+      [x, y] = closest.xy
+      ctx.lineTo(x,y)
+      ctx.stroke()
+      ctx.closePath()
+
+      drawDot.apply(null, prevClosest.rgb)
+      drawDot.apply(null, closest.rgb)
+      prevClosest = closest
+  return start + 1
+
+
+closestPointWithValueAndHue  = (points, value, hue) ->
+  potentials = points.filter (p) ->
+    [h, s, v] = p.hsv
+    return v == value
+
+  sorted = _.sortBy potentials, (p) ->
+    a = Math.abs(p.hsv[0] - hue)
+    if a > 180
+      a = 360 - a
+    return Math.abs(a)
+  return sorted[0]
+
+
 
 drawDot = (r, g, b) ->
   [h, s, v] = rgbToHsv(r, g, b)
+  r <<= 4
+  g <<= 4
+  b <<= 4
+
+  color = "rgb(#{r},#{g},#{b})"
+  width = 12
+  r = radius * s * v / 15
+  x = radius + r * Math.cos(h)
+  y = radius + r * Math.sin(h)
+
+  ctx.beginPath()
+  ctx.arc(x, y, width, 0, 2 * Math.PI, false)
+  ctx.fillStyle = color
+  ctx.fill()
+
+drawLine = (r, g, b) ->
+  [h, s, v] = rgbToHsv(r, g, b)
+
+  if( v != 1 )
+    return
+
   r <<= 4
   g <<= 4
   b <<= 4
@@ -51,41 +172,44 @@ drawDot = (r, g, b) ->
   r = radius * v
   x = radius + r * Math.cos(h)
   y = radius + r * Math.sin(h)
-  x -= width / 2
-  y -= width / 2
-  ctx.fillStyle = color
-  ctx.fillRect(x,y,width,width)
 
-draw = () ->
+  ctx.beginPath()
+  ctx.moveTo(x,y)
+  ctx.lineTo(radius,radius)
+  ctx.stroke()
+
+drawSomethingWhen = (fn, whenTrue) ->
+  for red in [0..0xF]
+    for green in [0..0xF]
+      for blue in [0..0xF]
+        if whenTrue(red, green, blue)
+          fn(red, green, blue)
+
+draw = (value) ->
   strokeSize = 3
   size = Math.min(window.innerWidth, window.innerHeight)
   ctx.canvas.width = ctx.canvas.height = size
   radius = (size - 2 * strokeSize) / 2
 
-  for mult in [0..16]
-    r = mult * (radius / 16)
+  ###
+  for mult in [0..15]
+    r = mult * (radius / 15)
     ctx.beginPath()
     ctx.arc(radius, radius, r, 0, 2 * Math.PI, false)
     ctx.lineWidth = strokeSize
     ctx.strokeStyle = "white"
     ctx.stroke()
+  ###
 
-  for red in [0..0xF]
-    for green in [0..0xF]
-      drawDot(red, green, 0)
+  valueMatches = (r, g, b) ->
+    return rgbToHsv(r, g, b)[2] == value
 
-  for green in [0..0xF]
-    for blue in [0..0xF]
-      drawDot(0, green, blue)
+  # drawSomethingWhen(drawLine, valueMatches)
+  drawSomethingWhen(drawDot, valueMatches)
 
-  for blue in [0..0xF]
-    for red in [0..0xF]
-      drawDot(red, 0, blue)
 
 document.addEventListener "DOMContentLoaded", () ->
   document.removeEventListener( "DOMContentLoaded", arguments.callee, false )
   ready()
-  draw()
 , false
 
-window.addEventListener("resize", draw, false)
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function() {
-  var canvas, ctx, draw, drawDot, radius, ready, rgbToHsv;
+  var canvas, closestPointWithValueAndHue, ctx, draw, drawDot, drawLine, drawPaths, drawSomethingWhen, radius, ready, rgbToHsv;
 
   canvas = null;
 
@@ -8,10 +8,12 @@
   radius = 500;
 
   rgbToHsv = function(r, g, b) {
+    /*
+      r = r / 0xF
+      g = g / 0xF
+      b = b / 0xF
+    */
     var delta, h, max, min, s, v;
-    r = r / 0xF;
-    g = g / 0xF;
-    b = b / 0xF;
     min = Math.min(r, g, b);
     max = Math.max(r, g, b);
     delta = max - min;
@@ -37,8 +39,102 @@
   };
 
   ready = function() {
+    var blue, green, hues, i, red, value;
     canvas = document.getElementById('wheel');
-    return ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d');
+    value = 0;
+    setInterval(function() {
+      draw(value);
+      value++;
+      if (value > 15) return value = 0;
+    }, 500);
+    return;
+    hues = [];
+    for (red = 0; 0 <= 0xF ? red <= 0xF : red >= 0xF; 0 <= 0xF ? red++ : red--) {
+      for (green = 0; 0 <= 0xF ? green <= 0xF : green >= 0xF; 0 <= 0xF ? green++ : green--) {
+        for (blue = 0; 0 <= 0xF ? blue <= 0xF : blue >= 0xF; 0 <= 0xF ? blue++ : blue--) {
+          hues.push(rgbToHsv(red, green, blue)[0]);
+        }
+      }
+    }
+    console.log(_.uniq(hues));
+    i = 0;
+    return setInterval(function() {
+      return i = drawPaths(i);
+    }, 100);
+  };
+
+  drawPaths = function(start) {
+    var allPoints, outerPoints, vObj, vs;
+    canvas.width = canvas.width;
+    allPoints = [];
+    outerPoints = [];
+    drawSomething(function(r, g, b) {
+      var h, obj, s, v, x, y, _ref;
+      _ref = rgbToHsv(r, g, b), h = _ref[0], s = _ref[1], v = _ref[2];
+      x = radius + (radius * s / 0xF) * Math.cos(h);
+      y = radius + (radius * s / 0xF) * Math.sin(h);
+      obj = {
+        hsv: [h, s, v],
+        rgb: [r, g, b],
+        xy: [x, y]
+      };
+      return allPoints.push(obj);
+    });
+    vObj = {};
+    allPoints.forEach(function(point) {
+      var h, s, v, _ref;
+      _ref = point.hsv, h = _ref[0], s = _ref[1], v = _ref[2];
+      return vObj[v] = true;
+    });
+    vs = Object.keys(vObj);
+    vs.sort();
+    vs = vs.reverse();
+    allPoints.forEach(function(point) {
+      var h, s, v, _ref;
+      _ref = point.hsv, h = _ref[0], s = _ref[1], v = _ref[2];
+      if (v === 15) return outerPoints.push(point);
+    });
+    outerPoints = _.sortBy(outerPoints, function(p) {
+      return p.hsv[0];
+    });
+    console.log(outerPoints.length);
+    if (start > outerPoints.length - 2) start = 0;
+    outerPoints.slice(start, start + 1).forEach(function(p) {
+      var closest, i, prevClosest, x, y, _ref, _ref2;
+      prevClosest = closestPointWithValueAndHue(allPoints, p.hsv[2] - 1, p.hsv[0]);
+      for (i = 2; i <= 15; i++) {
+        closest = closestPointWithValueAndHue(allPoints, p.hsv[2] - i, p.hsv[0]);
+        if (!(closest != null)) return;
+        ctx.beginPath();
+        _ref = prevClosest.xy, x = _ref[0], y = _ref[1];
+        ctx.moveTo(x, y);
+        _ref2 = closest.xy, x = _ref2[0], y = _ref2[1];
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        ctx.closePath();
+        drawDot.apply(null, prevClosest.rgb);
+        drawDot.apply(null, closest.rgb);
+        prevClosest = closest;
+      }
+    });
+    return start + 1;
+  };
+
+  closestPointWithValueAndHue = function(points, value, hue) {
+    var potentials, sorted;
+    potentials = points.filter(function(p) {
+      var h, s, v, _ref;
+      _ref = p.hsv, h = _ref[0], s = _ref[1], v = _ref[2];
+      return v === value;
+    });
+    sorted = _.sortBy(potentials, function(p) {
+      var a;
+      a = Math.abs(p.hsv[0] - hue);
+      if (a > 180) a = 360 - a;
+      return Math.abs(a);
+    });
+    return sorted[0];
   };
 
   drawDot = function(r, g, b) {
@@ -48,47 +144,54 @@
     g <<= 4;
     b <<= 4;
     color = "rgb(" + r + "," + g + "," + b + ")";
+    width = 12;
+    r = radius * s * v / 15;
+    x = radius + r * Math.cos(h);
+    y = radius + r * Math.sin(h);
+    ctx.beginPath();
+    ctx.arc(x, y, width, 0, 2 * Math.PI, false);
+    ctx.fillStyle = color;
+    return ctx.fill();
+  };
+
+  drawLine = function(r, g, b) {
+    var color, h, s, v, width, x, y, _ref;
+    _ref = rgbToHsv(r, g, b), h = _ref[0], s = _ref[1], v = _ref[2];
+    if (v !== 1) return;
+    r <<= 4;
+    g <<= 4;
+    b <<= 4;
+    color = "rgb(" + r + "," + g + "," + b + ")";
     width = 16;
     r = radius * v;
     x = radius + r * Math.cos(h);
     y = radius + r * Math.sin(h);
-    x -= width / 2;
-    y -= width / 2;
-    ctx.fillStyle = color;
-    return ctx.fillRect(x, y, width, width);
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(radius, radius);
+    return ctx.stroke();
   };
 
-  draw = function() {
-    var blue, green, mult, r, red, size, strokeSize, _results;
-    strokeSize = 3;
-    size = Math.min(window.innerWidth, window.innerHeight);
-    ctx.canvas.width = ctx.canvas.height = size;
-    radius = (size - 2 * strokeSize) / 2;
-    for (mult = 0; mult <= 16; mult++) {
-      r = mult * (radius / 16);
-      ctx.beginPath();
-      ctx.arc(radius, radius, r, 0, 2 * Math.PI, false);
-      ctx.lineWidth = strokeSize;
-      ctx.strokeStyle = "white";
-      ctx.stroke();
-    }
-    for (red = 0; 0 <= 0xF ? red <= 0xF : red >= 0xF; 0 <= 0xF ? red++ : red--) {
-      for (green = 0; 0 <= 0xF ? green <= 0xF : green >= 0xF; 0 <= 0xF ? green++ : green--) {
-        drawDot(red, green, 0);
-      }
-    }
-    for (green = 0; 0 <= 0xF ? green <= 0xF : green >= 0xF; 0 <= 0xF ? green++ : green--) {
-      for (blue = 0; 0 <= 0xF ? blue <= 0xF : blue >= 0xF; 0 <= 0xF ? blue++ : blue--) {
-        drawDot(0, green, blue);
-      }
-    }
+  drawSomethingWhen = function(fn, whenTrue) {
+    var blue, green, red, _results;
     _results = [];
-    for (blue = 0; 0 <= 0xF ? blue <= 0xF : blue >= 0xF; 0 <= 0xF ? blue++ : blue--) {
+    for (red = 0; 0 <= 0xF ? red <= 0xF : red >= 0xF; 0 <= 0xF ? red++ : red--) {
       _results.push((function() {
         var _results2;
         _results2 = [];
-        for (red = 0; 0 <= 0xF ? red <= 0xF : red >= 0xF; 0 <= 0xF ? red++ : red--) {
-          _results2.push(drawDot(red, 0, blue));
+        for (green = 0; 0 <= 0xF ? green <= 0xF : green >= 0xF; 0 <= 0xF ? green++ : green--) {
+          _results2.push((function() {
+            var _results3;
+            _results3 = [];
+            for (blue = 0; 0 <= 0xF ? blue <= 0xF : blue >= 0xF; 0 <= 0xF ? blue++ : blue--) {
+              if (whenTrue(red, green, blue)) {
+                _results3.push(fn(red, green, blue));
+              } else {
+                _results3.push(void 0);
+              }
+            }
+            return _results3;
+          })());
         }
         return _results2;
       })());
@@ -96,12 +199,30 @@
     return _results;
   };
 
+  draw = function(value) {
+    var size, strokeSize, valueMatches;
+    strokeSize = 3;
+    size = Math.min(window.innerWidth, window.innerHeight);
+    ctx.canvas.width = ctx.canvas.height = size;
+    radius = (size - 2 * strokeSize) / 2;
+    /*
+      for mult in [0..15]
+        r = mult * (radius / 15)
+        ctx.beginPath()
+        ctx.arc(radius, radius, r, 0, 2 * Math.PI, false)
+        ctx.lineWidth = strokeSize
+        ctx.strokeStyle = "white"
+        ctx.stroke()
+    */
+    valueMatches = function(r, g, b) {
+      return rgbToHsv(r, g, b)[2] === value;
+    };
+    return drawSomethingWhen(drawDot, valueMatches);
+  };
+
   document.addEventListener("DOMContentLoaded", function() {
     document.removeEventListener("DOMContentLoaded", arguments.callee, false);
-    ready();
-    return draw();
+    return ready();
   }, false);
-
-  window.addEventListener("resize", draw, false);
 
 }).call(this);
